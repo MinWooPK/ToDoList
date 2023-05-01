945ae3c84bd64929ab56741d2dea78cd483f7ba3
MinWooPK <min.park@etpay.com>
2023-05-01 20:50:21 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState, useRef } from 'react';
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "npx Create react-my-app Prueba", done: false },
+    { id: 2, text: "cd Prueba", done: false },
+    { id: 3, text: "npm start", done: false }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Tasks !!</h2>
+      <ul>
+        <TodoList todos={todos} setTodos={setTodos} />
+        <AddTodo setTodos={setTodos} />
+      </ul>
     </div>
+  )
+}
+function TodoList({ todos, setTodos }) {
+
+  function handleToggleTodo(todo) {
+
+    const updatedTodos = todos.map((clickedTodo) =>
+      clickedTodo.id === todo.id ? { ...clickedTodo, done: !clickedTodo.done } : clickedTodo
+    );
+    setTodos(updatedTodos)
+  }
+
+
+  //Meter en ternario
+  return (
+    !todos.length ? (
+      <p style={{ color: "purple" }}> No hay mas tareas !!!</p>
+    ) : (
+      
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}
+            onDoubleClick={() => handleToggleTodo(todo)}
+            style={{ textDecoration: todo.done ? "line-through" : "" }}
+          >
+            {todo.text}
+            <DeleteTodo todo={todo} setTodos={setTodos} />
+          </li>
+        ))}
+      </ul>
+    )
   );
 }
 
-export default App;
+
+
+function DeleteTodo({ todo, setTodos }) {
+
+  function handleDeleteTodo() {
+
+    const confirmed = window.confirm('¿Estas seguro de borrar?');
+    if (confirmed) {
+      setTodos((prevTodos) => {
+
+        return prevTodos.filter((element) => element.id !== todo.id);
+        //no llamar las variables con una letra (todo Item)
+      })
+    }
+  }
+  return (
+    <span onClick={handleDeleteTodo}
+      role="button"
+      style={{
+        color: "red",
+        fontWeight: "bold",
+        marginLeft: 10,
+        cursor: "pointer"
+      }}
+    >
+      Done
+    </span>
+  )
+}
+
+
+
+function AddTodo({ setTodos }) {
+  const inputRef = useRef();
+  function handleAddTodo(event) {
+
+    event.preventDefault()
+    const text = event.target.elements.addTodo.value;
+    const todo = {
+      id: Math.random(),
+      text,
+      done: false
+    };
+
+    setTodos((prevTodos) => {
+      return [...prevTodos, todo]
+    })
+    inputRef.current.value = ""
+  }
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input placeholder='Tareas a Realizar !!!!!' ref={inputRef} name="addTodo" />
+      <button type='submit'>añadir</button>
+    </form>
+  )
+}
+
+
+
